f558da3f7efc83b3548bdc297aabd1e9cc4bb116
Ubuntu <ubuntu@ip-172-31-31-95.us-west-2.compute.internal>
2015-03-10 02:10:07 +0000

    $0

---
+++ github_sync.sh
@@ -0,0 +1,14 @@
+find . -name '*.pid' -exec git rm {} \; \
+&& echo "pid is deleted!" \
+&& git add . \
+&& echo "add is completed!" \
+&& git rm --cached -r ./apps/magento/htdocs/var/session \
+&& echo "session is deleted!" \
+&& git rm --cached -r ./apache2/var/cache/mod_pagespeed \
+&& echo "mod_pagespedd is deleted!" \
+&& echo "git log" \
+&& echo "log is ended!" \
+&& git status \
+&& echo "argument0:"$0 \
+&& git commit -m '$0' \
+&& git push -u oms1226 taewolInAWS
