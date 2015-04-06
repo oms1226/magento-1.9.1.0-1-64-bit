c9c813389af71f47698ac4e6a237ed568966cf73
Ubuntu <ubuntu@ip-172-31-31-95.us-west-2.compute.internal>
2015-04-06 12:25:23 +0900

    setup

--- github_sync.sh
+++ github_sync.sh
@@ -16,9 +16,11 @@ fi
 && echo "session is deleted!" \
 && git rm --cached -r ./apache2/var/cache/mod_pagespeed \
 && echo "mod_pagespedd is deleted!" \
+&& git rm --cached -r ./apps/magento/htdocs/var/report \
+&& echo "report is deleted!" \
 && echo "git log" \
 && echo "log is ended!" \
 && git status \
 && echo "argument0:${1}" \
 && git commit -m "${1}" \
-&& git push -u oms1226 taewolInAWS
+&& git push -u oms1226 canopyInAWS
